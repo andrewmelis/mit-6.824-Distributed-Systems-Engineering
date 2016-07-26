c2a22227c4a99b0dbe29b22bd484b9d7f3d1eb92
Andrew Melis <andrewmelis@gmail.com>
2016-07-26 11:17:21 -0500

    add cleanup to provided test-wc script

--- src/main/test-wc.sh
+++ src/main/test-wc.sh
@@ -1,11 +1,26 @@
 #!/bin/bash
+
+cleanup() {
+  echo "cleaning up temporary files..."
+  rm mrtmp.*
+}
+
+while getopts "ca:b" flag; do
+  case "$flag" in
+    c) trap cleanup EXIT
+      ;;
+  esac
+done
+
 go run wc.go master sequential pg-*.txt
 sort -n -k2 mrtmp.wcseq | tail -10 | diff - mr-testout.txt > diff.out
 if [ -s diff.out ]
 then
-echo "Failed test. Output should be as in mr-testout.txt. Your output differs as follows (from diff.out):" > /dev/stderr
+  echo "Failed test. Output should be as in mr-testout.txt. Your output differs as follows (from diff.out):" > /dev/stderr
   cat diff.out
+  exit 1
 else
   echo "Passed test" > /dev/stderr
+  exit 0
 fi
 
