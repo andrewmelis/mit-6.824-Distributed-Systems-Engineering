58a00d15b8aa76772a1f9fa57d78dc9ad5873009
andrew melis <andrewmelis@gmail.com>
2016-08-08 20:40:27 -0500

    remove whitespace

--- src/raft/raft.go
+++ src/raft/raft.go
@@ -23,8 +23,6 @@ import "labrpc"
 // import "bytes"
 // import "encoding/gob"
 
-
-
 //
 // as each Raft peer becomes aware that successive log entries are
 // committed, the peer should send an ApplyMsg to the service (or
