d93655e59b61c918f52b8935a9508a3e5399018f
andrew melis <andrewmelis@gmail.com>
2016-08-14 21:07:34 -0500

    add microsecond resolution to DPrintf

--- src/raft/util.go
+++ src/raft/util.go
@@ -3,10 +3,12 @@ package raft
 import "log"
 
 // Debugging
-const Debug = 0
+const Debug = 1
 
 func DPrintf(format string, a ...interface{}) (n int, err error) {
+
 	if Debug > 0 {
+		log.SetFlags(log.LstdFlags | log.Lmicroseconds)
 		log.Printf(format, a...)
 	}
 	return
