1a6b8cc7aedcc2d44c99749c5647c49b1f451086
Andrew Melis <andrewmelis@gmail.com>
2016-08-23 17:39:35 -0500

    build out naive replicate logic

--- src/raft/replicate.go
+++ src/raft/replicate.go
@@ -1,47 +1,68 @@
 package raft
 
 // replicates, then applies -- better name to encompass both steps?
-// func replicateLog(command interface{}) {
-// 	replicatedCh := make(chan struct{})
+func (rf *Raft) replicateLog(command interface{}) {
+	doneCh := make(chan struct{})
 
-// 	// go replicate(command, replicatedCh) // push go down to replicate, or go here?
+	go rf.replicate(command, doneCh) // push go down to replicate, or go here?
 
-// 	select {
-// 	case <-replicatedCh: // block until replicated. IRL probably timeout
-// 		// go commit(command)
+	<-doneCh // block until replicated. IRL probably timeout
+	DPrintf("leader %d successfully replicated command %v\n", rf.me, command)
 
-// 		// case time.After(foobar):
-// 		// 	DPrintf("this timed out\n")
-// 	}
+	// commit now (update commitIndex, then apply)
+	rf.commitIndex++
 
-// }
+	// extract
+	for rf.commitIndex > rf.lastApplied {
+		rf.lastApplied++
+		applyMsg := ApplyMsg{Index: rf.lastApplied, Command: rf.log[rf.lastApplied].Command}
+		rf.applyCh <- applyMsg
+		DPrintf("peer %d applied msg: %+v\n", rf.me, applyMsg)
+	}
+}
 
-// func (rf *Raft) replicate(command interface{}, doneCh chan<- struct{}) {
-// 	nextLog := LogEntry{Command: command, Term: rf.currentTerm}
-// 	rf.log = append(rf.log, nextLog)
+func (rf *Raft) replicate(command interface{}, doneCh chan struct{}) {
+	nextLog := LogEntry{Command: command, Term: rf.currentTerm}
+	rf.log = append(rf.log, nextLog)
 
-// 	peerConfirmationCh := make(chan struct{})
-// 	go replicateWorker(peerConfirmationCh, doneCh)
+	peerConfirmationCh := make(chan int)
+	go replicateWorker(rf.majority(), peerConfirmationCh, doneCh)
 
-// 	for i := range rf.peers {
-// 		if i == rf.me {
-// 			continue
-// 		}
+	for i := range rf.peers {
+		if i == rf.me {
+			peerConfirmationCh <- rf.me
+			continue
+		}
 
-// 		// if ok := rf.sendAppendEntries
-// 		// boilerplate
-// 		// appendEntries RPC
-// 	}
-// }
+		go func(peerIndex int) {
+			args := AppendEntriesArgs{Term: rf.currentTerm, LeaderId: rf.me, Entries: []LogEntry{nextLog}, LeaderCommit: rf.commitIndex}
+			reply := AppendEntriesReply{}
+			if ok := rf.sendAppendEntries(peerIndex, args, &reply); !ok {
+				// need to retry here or something?
+				DPrintf("append RPC from leader %d to peer %d failed!\n", rf.me, i)
+			}
 
-// func replicateWorker(majority int, peerConfirmationCh <-chan struct{}, doneCh chan<- struct{}) {
-// 	var confirmedPeers int
+			if reply.Success { // TODO push this down to just `peerConfirmationCh <- reply` ???
+				DPrintf("leader %d got successful append entries response from peer %d\n", rf.me, peerIndex)
+				select {
+				case <-doneCh:
+					return // already replicated, don't care
+				case peerConfirmationCh <- peerIndex: // probably should just send the reply here and let
+					// TODO bookkeeping or something
+				}
+			}
+		}(i)
+	}
+}
 
-// 	for range peerConfirmationCh {
-// 		confirmedPeers++
-// 		if confirmedPeers > majority {
-// 			close(doneCh)
-// 			return
-// 		}
-// 	}
-// }
+func replicateWorker(majority int, peerConfirmationCh <-chan int, doneCh chan<- struct{}) {
+	var confirmedPeers int
+
+	for range peerConfirmationCh {
+		confirmedPeers++
+		if confirmedPeers > majority {
+			close(doneCh)
+			return
+		}
+	}
+}
