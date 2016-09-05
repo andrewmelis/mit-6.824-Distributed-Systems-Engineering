f2dad26a19dbe6e828580410c37ac0a68bc02400
andrew melis <andrewmelis@gmail.com>
2016-09-04 19:04:36 -0500

    alter for loop log msg for clarity

--- src/raft/follower.go
+++ src/raft/follower.go
@@ -10,10 +10,10 @@ func (rf *Raft) beFollower() {
 			DPrintf("peer %d done being follower\n", rf.me)
 			return // better way to do this?
 		case <-rf.requestVoteCh:
-			DPrintf("peer %d received a request vote RPC\n", rf.me)
+			DPrintf("peer %d concluded a request vote RPC\n", rf.me)
 			rf.resetCh <- struct{}{}
 		case <-rf.appendEntriesCh:
-			DPrintf("peer %d received an append entries RPC\n", rf.me)
+			DPrintf("peer %d concluded an append entries RPC\n", rf.me)
 			rf.resetCh <- struct{}{}
 		}
 	}
